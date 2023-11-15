6cae67d810694683184e62502961f330ab80346a
sorask <45480620+sorask@users.noreply.github.com>
2023-11-15 15:17:05 +0300

    Add files via upload

---
+++ ClimateApp.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.util.ArrayList;
+
+import measurements.ClimateRecord;
+
+public class ClimateApp {
+    public static void main(String[] args) {
+        ClimateRecord climateRecord = new ClimateRecord();
+        /*
+         * ArrayList<Double> averageCountryTemp =
+         * climateRecord.getAverageCountryTemperature("Germany", "Celsius", 2021);
+         * ArrayList<Double> averageCityTemp =
+         * climateRecord.getAverageCityTemperature("Berlin", "Fahrenheit", 2022);
+         * double averageWindSpeed = climateRecord.getAverageCityWindSpeed("Izmir",
+         * "km/h", "March");
+         * double averageHumidity = climateRecord.getAverageCityHumidity("Paris");
+         * int intensityCount = climateRecord.getRadiationIntensityCount("Berlin", 2022,
+         * "medium");
+         * double feltTemperature = climateRecord.calculateFeltTemperature("Paris",
+         * "July", 2021);
+         */
+    }
+}
